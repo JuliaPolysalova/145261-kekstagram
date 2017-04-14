7ea8426e62e9130935c4ec8d03e8676b1386755d
JuliaPolysalova <uliaska2@mail.ru>
2017-04-14 04:33:41 +0300

    Update pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -61,7 +61,7 @@ function showOverlay(photoData) {
     galleryOverlay.querySelector('.gallery-overlay-image').src = photoData.url;
     galleryOverlay.querySelector('.likes-count').textContent = photoData.likes;
     galleryOverlay.querySelector('.comments-count').textContent = photoData.comments.length;
-};
+}
 
 
 function init() {
@@ -72,3 +72,68 @@ function init() {
 }
 
 init();
+
+//task2
+var picturesContainerAll = document.querySelectorAll('.pictures');
+var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
+
+function onEscPress(evt) {
+    if (evt.keyCode === 27) {
+        closePicture();
+        }
+}
+
+//picturesContainerAll.forEach(function(x) {return x;});
+
+function openPicture() {
+    galleryOverlay.classList.remove('invisible');
+    document.addEventListener('keydown', onEscPress);
+}
+
+function closePicture() {
+    galleryOverlay.classList.add('invisible');
+    document.removeEventListener('keydown', onEscPress);
+}
+
+//При нажатии на любой из элементов .picture должен показываться элемент .gallery-overlay с подробным описанием картинки
+for (var i = 0; i < picturesContainerAll.length; i++)
+    picturesContainerAll[i].addEventListener('click', function(){
+    openPicture();
+});
+
+//При нажатии на элемент .gallery-overlay-close элемент .gallery-overlay должен скрываться
+galleryOverlayClose.addEventListener('click', function(){
+    closePicture();
+});
+
+var ENTER_KEY_CODE = 13;
+
+function isActivationEvent(evt) {
+    return evt.keyCode === ENTER_KEY_CODE;
+}
+
+for (var i = 0; i < picturesContainerAll.length; i++)
+    picturesContainerAll[i].addEventListener('keydown', function(evt) {
+    if (isActivationEvent(evt)) {
+        openPicture();
+    }
+});
+
+galleryOverlayClose.addEventListener('keydown', function(evt) {
+    if (isActivationEvent(evt)){
+        closePicture();
+    };
+});
+
+//При изменении значения поля загрузки фотографии #upload-file в форме #upload-select-image, показывается форма кадрирования изображения, а форма загрузки скрывается
+var uploadOverlay = document.querySelector('.upload-overlay');
+uploadOverlay.classList.add('invisible');
+
+var uploadForm = document.querySelector('#upload-select-image');
+uploadForm.classList.remove('invisible');
+
+var uploadFormCancel = document.querySelector('.upload-form-cancel');
+uploadFormCancel.addEventListener('click', function () {
+    uploadOverlay.classList.remove('invisible');
+    uploadForm.classList.add('invisible');
+})
