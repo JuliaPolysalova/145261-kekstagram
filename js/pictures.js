19777ea8f18fd3bda02d8929cd377c2f018aa0c8
JuliaPolysalova <uliaska2@mail.ru>
2017-04-18 22:49:24 +0300

    1

--- js/pictures.js
+++ js/pictures.js
@@ -4,7 +4,18 @@ var NUMBER_PHOTOS = 25;
 var galleryOverlay = document.querySelector('.gallery-overlay');
 var picturesContainer = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture-template').content;
+var uploadOverlay = document.querySelector('.upload-overlay');
 
+var picturesContainerAll = null;
+var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
+
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
+var uploadForm = document.querySelector('#upload-select-image'); ///BBEPX
+var uploadFormCancel = document.querySelector('.upload-form-cancel'); //BBEPX
+var uploadFormSubmit = document.querySelector('.upload-form-submit'); //вверх
+var uploadFormDescription = document.querySelector('.upload-form-description'); //BBEPX
 
 function getPhotosObjects(photosNumber) {
     var commentsArray = [
@@ -55,20 +66,110 @@ function addPhotos(containerElement, photoDataArray) {
     containerElement.appendChild(fragment);
 }
 
+function getUniqueClickListener(photoData) {
+    return function(evt) {
+        evt.preventDefault();
+        openPicture(photoData);
+    }
+}
 
-function showOverlay(photoData) {
-    galleryOverlay.classList.remove('invisible');
-    galleryOverlay.querySelector('.gallery-overlay-image').src = photoData.url;
-    galleryOverlay.querySelector('.likes-count').textContent = photoData.likes;
-    galleryOverlay.querySelector('.comments-count').textContent = photoData.comments.length;
-};
+function initEventListenersForPhotos (photoObjectsData) {
+    //При нажатии на любой из элементов .picture должен показываться элемент .gallery-overlay с подробным описанием картинки
+    for (var i = 0; i < picturesContainerAll.length; i++) {
+        picturesContainerAll[i].addEventListener('click', getUniqueClickListener(photoObjectsData[i]) );
+    }
+}
 
+function hideUploadOverlay(evt) {
+    uploadOverlay.classList.add('invisible'); //закрытие формы кадрирования
+}
+
+function showUploadOverlay(evt) {
+    uploadOverlay.classList.remove('invisible'); //открытие ф. кадрирования
+    document.addEventListener('keydown', onEscPressUpload);
+}
+
+function onEscPressUpload(evt) {
+    if (evt.keyCode === ESCAPE_KEY_CODE) {
+        hideUploadOverlay();
+    }
+}
 
 function init() {
     var photosObjectData = getPhotosObjects(NUMBER_PHOTOS);
-    document.querySelector('.upload-overlay').classList.add('invisible');
+    hideUploadOverlay();
     addPhotos(picturesContainer, photosObjectData);
-    showOverlay(photosObjectData[0]);
+    openPicture(photosObjectData[0]);
+
+    picturesContainerAll = document.querySelectorAll('.picture');
+    initEventListenersForPhotos(photosObjectData);
+
+    document.getElementById('upload-file').addEventListener('change', showUploadOverlay);
 }
 
 init();
+
+//task2
+function onEscPress(evt) {
+    if (evt.keyCode === ESCAPE_KEY_CODE) {
+        closePicture();
+    }
+}
+
+function openPicture(pictureData) {
+    document.addEventListener('keydown', onEscPress);
+
+    galleryOverlay.querySelector('.gallery-overlay-image').src = pictureData.url;
+    galleryOverlay.querySelector('.likes-count').textContent = pictureData.likes;
+    galleryOverlay.querySelector('.comments-count').textContent = pictureData.comments.length;
+    galleryOverlay.classList.remove('invisible');
+}
+
+function closePicture() {
+    galleryOverlay.classList.add('invisible');
+    document.removeEventListener('keydown', onEscPress);
+}
+
+
+//При нажатии на элемент .gallery-overlay-close элемент .gallery-overlay должен скрываться
+galleryOverlayClose.addEventListener('click', function(){
+    closePicture();
+});
+
+function isActivationEvent(evt) {
+    return evt.keyCode === ENTER_KEY_CODE;
+}
+
+galleryOverlayClose.addEventListener('keydown', function(evt) {
+    if (isActivationEvent(evt)){
+        closePicture();
+    };
+});
+
+uploadForm.classList.remove('invisible');
+
+uploadFormCancel.addEventListener('click', function() {
+    hideUploadOverlay();
+});
+uploadFormCancel.addEventListener('keydown', function(evt) {
+    if (isActivationEvent(evt)){
+        hideUploadOverlay();
+    };
+});
+
+
+uploadFormSubmit.addEventListener('click', function() {
+    hideUploadOverlay();
+})
+
+uploadFormSubmit.addEventListener('keydown', function(evt) {
+    if (isActivationEvent(evt)){
+        hideUploadOverlay();
+    };
+});
+
+uploadFormDescription.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === ESCAPE_KEY_CODE) {
+        evt.stopPropagation();
+    }
+});
