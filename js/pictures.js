184c6b20b13bbb06e194bddf194ee3be474406bc
JuliaPolysalova <uliaska2@mail.ru>
2017-04-19 23:17:37 +0300

    Update pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -4,7 +4,30 @@ var NUMBER_PHOTOS = 25;
 var galleryOverlay = document.querySelector('.gallery-overlay');
 var picturesContainer = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture-template').content;
+var uploadOverlay = document.querySelector('.upload-overlay');
 
+var picturesContainerAll = null;
+var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
+
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
+var uploadForm = document.querySelector('#upload-select-image'); ///BBEPX
+var uploadFormCancel = document.querySelector('.upload-form-cancel'); //BBEPX
+var uploadFormSubmit = document.querySelector('.upload-form-submit'); //вверх
+var uploadFormDescription = document.querySelector('.upload-form-description'); //BBEPX
+
+// блок формы кадрирования
+var filterForm = document.querySelector('.upload-filter');
+var filterFormMinusBtn = filterForm.querySelector('.upload-resize-controls-button-dec');
+var filterFormPlusBtn = filterForm.querySelector('.upload-resize-controls-button-inc');
+var filterFormPreview = filterForm.querySelector('.filter-image-preview');
+var filterFormResizeInput = filterForm.querySelector('input[type="text"]');
+
+//размер изображения
+var MAX_RESIZE = '100%';
+var MIN_RESIZE = '25%';
+var STEP_RESIZE = 25;
 
 function getPhotosObjects(photosNumber) {
     var commentsArray = [
@@ -43,7 +66,6 @@ function createPhotoElement(photoData) {
     return photoElement;
 }
 
-
 function addPhotos(containerElement, photoDataArray) {
     var fragment = document.createDocumentFragment();
 
@@ -55,20 +77,141 @@ function addPhotos(containerElement, photoDataArray) {
     containerElement.appendChild(fragment);
 }
 
+function getUniqueClickListener(photoData) {
+    return function(evt) {
+        evt.preventDefault();
+        openPicture(photoData);
+    }
+}
 
-function showOverlay(photoData) {
-    galleryOverlay.classList.remove('invisible');
-    galleryOverlay.querySelector('.gallery-overlay-image').src = photoData.url;
-    galleryOverlay.querySelector('.likes-count').textContent = photoData.likes;
-    galleryOverlay.querySelector('.comments-count').textContent = photoData.comments.length;
-};
+function initEventListenersForPhotos (photoObjectsData) {
+    //При нажатии на любой из элементов .picture должен показываться элемент .gallery-overlay с подробным описанием картинки
+    for (var i = 0; i < picturesContainerAll.length; i++) {
+        picturesContainerAll[i].addEventListener('click', getUniqueClickListener(photoObjectsData[i]) );
+    }
+}
 
+function hideUploadOverlay(evt) {
+    uploadOverlay.classList.add('invisible'); //закрытие формы кадрирования
+}
+
+function showUploadOverlay(evt) {
+    uploadOverlay.classList.remove('invisible'); //открытие ф. кадрирования
+    document.addEventListener('keydown', onEscPressUpload);
+}
+
+function onEscPressUpload(evt) {
+    if (evt.keyCode === ESCAPE_KEY_CODE) {
+        hideUploadOverlay();
+    }
+}
 
 function init() {
     var photosObjectData = getPhotosObjects(NUMBER_PHOTOS);
-    document.querySelector('.upload-overlay').classList.add('invisible');
+    hideUploadOverlay();
     addPhotos(picturesContainer, photosObjectData);
-    showOverlay(photosObjectData[0]);
+    openPicture(photosObjectData[0]);
+
+    picturesContainerAll = document.querySelectorAll('.picture');
+    initEventListenersForPhotos(photosObjectData);
+
+    document.getElementById('upload-file').addEventListener('change', showUploadOverlay);
 }
 
 init();
+
+//task2
+function onEscPress(evt) {
+    if (evt.keyCode === ESCAPE_KEY_CODE) {
+        closePicture();
+    }
+}
+
+function openPicture(pictureData) {
+    document.addEventListener('keydown', onEscPress);
+
+    galleryOverlay.querySelector('.gallery-overlay-image').src = pictureData.url;
+    galleryOverlay.querySelector('.likes-count').textContent = pictureData.likes;
+    galleryOverlay.querySelector('.comments-count').textContent = pictureData.comments.length;
+    galleryOverlay.classList.remove('invisible');
+}
+
+function closePicture() {
+    galleryOverlay.classList.add('invisible');
+    document.removeEventListener('keydown', onEscPress);
+}
+
+
+//При нажатии на элемент .gallery-overlay-close элемент .gallery-overlay должен скрываться
+galleryOverlayClose.addEventListener('click', function(){
+    closePicture();
+});
+
+function isActivationEvent(evt) {
+    return evt.keyCode === ENTER_KEY_CODE;
+}
+
+galleryOverlayClose.addEventListener('keydown', function(evt) {
+    if (isActivationEvent(evt)){
+        closePicture();
+    };
+});
+
+uploadForm.classList.remove('invisible');
+
+uploadFormCancel.addEventListener('click', function() {
+    hideUploadOverlay();
+});
+uploadFormCancel.addEventListener('keydown', function(evt) {
+    if (isActivationEvent(evt)){
+        hideUploadOverlay();
+    };
+});
+
+
+uploadFormSubmit.addEventListener('click', function() {
+    hideUploadOverlay();
+})
+
+uploadFormSubmit.addEventListener('keydown', function(evt) {
+    if (isActivationEvent(evt)){
+        hideUploadOverlay();
+    };
+});
+
+uploadFormDescription.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === ESCAPE_KEY_CODE) {
+        evt.stopPropagation();
+    }
+});
+
+//Изменение масштаба изображения
+filterFormPlusBtn.addEventListener('click', function(evt) {
+    setScale(MAX_RESIZE, 1);
+});
+
+filterFormMinusBtn.addEventListener('click', function(evt){
+    setScale(MIN_RESIZE, 0);
+});
+
+function setScale(sizeLimit, sign) {
+    var stepSize;
+    var sizeValue;
+    if (filterFormResizeInput.value !== sizeLimit) {
+        stepSize = sign ? STEP_RESIZE : -STEP_RESIZE;
+        sizeValue = parseInt(filterFormResizeInput.value, 10) + stepSize;
+        filterFormResizeInput.setAttribute('value', sizeValue + '%');
+        filterFormPreview.style.transform = 'scale(' + sizeValue / 100 + ')';
+    }
+}
+
+// установка фильтра
+function setFilter(evt) {
+    if (evt.target.checked) {
+        filterFormPreview.className = 'filter-image-preview filter-' + evt.target.value;
+    }
+}
+
+filterControls.addEventListener('click', function(evt) {
+    setFilter(evt);
+});
