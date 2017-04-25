98e0b948163d15affbc4184fb0604b7d81adf3d1
JuliaPolysalova <uliaska2@mail.ru>
2017-04-26 00:38:20 +0300

    Update pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -1,51 +1,21 @@
 'use strict';
 
-(function () {
-    var picturesContainer = document.querySelector('.pictures');
+window.picture = (function () {
+    
     var pictureTemplate = document.querySelector('#picture-template').content;
+    
+    function createPhotoElement(photoData) {
+        var photoElement = pictureTemplate.cloneNode(true);
 
-    var picturesContainerAll = null;
-    var galleryOverlay = document.querySelector('.gallery-overlay');
-    var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
+        photoElement.querySelector('img').src = photoData.url;
+        photoElement.querySelector('.picture-comments').textContent = photoData.comments.length;
+        photoElement.querySelector('.picture-likes').textContent = photoData.likes;
 
-    function onEscPress(evt) {
-        if (window.isEscape(evt)) {
-            closePicture();
-        }
+        return photoElement;
+    }
 
-        var NUMBER_PHOTOS = 25;
-        var galleryOverlay = document.querySelector('.gallery-overlay');
-        var picturesContainer = document.querySelector('.pictures');
-        var pictureTemplate = document.querySelector('#picture-template').content;
-        var uploadOverlay = document.querySelector('.upload-overlay');
+    return {
+        createPhotoElement : createPhotoElement
+    }
 
-        var picturesContainerAll = null;
-        var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
-
-        function openPicture(pictureData) {
-            document.addEventListener('keydown', onEscPress);
-
-            galleryOverlay.querySelector('.gallery-overlay-image').src = pictureData.url;
-            galleryOverlay.querySelector('.likes-count').textContent = pictureData.likes;
-            galleryOverlay.querySelector('.comments-count').textContent = pictureData.comments.length;
-            galleryOverlay.classList.remove('invisible');
-        }
-
-        function closePicture() {
-            galleryOverlay.classList.add('invisible');
-            document.removeEventListener('keydown', onEscPress);
-        }
-
-//При нажатии на элемент .gallery-overlay-close элемент .gallery-overlay должен скрываться
-        galleryOverlayClose.addEventListener('click', function () {
-            closePicture();
-        });
-
-        galleryOverlayClose.addEventListener('keydown', function (evt) {
-            if (window.isEnter(evt)) {
-                closePicture();
-            }
-            ;
-        });
-
-    })();
+})();
