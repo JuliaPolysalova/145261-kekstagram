0e5cffad445a67d907133597f722a7aca93526a5
JuliaPolysalova <uliaska2@mail.ru>
2017-04-26 01:00:01 +0300

    module5-task1

--- js/gallery.js
+++ js/gallery.js
@@ -1,49 +1,15 @@
 'use strict';
 
 (function () {
-    var NUMBER_PHOTOS = 25;
 
-    function getPhotosObjects(photosNumber) {
-        var commentsArray = [
-            'Всё отлично!',
-            'В целом всё неплохо. Но не всё.',
-            'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально.',
-            'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-            'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-            'Лица у людей на фотке перекошены, как-будто их избивают. Как можно было поймать такой неудачный момент?!'
-        ];
-
-        var photoArray = [];
-
-        for (var i = 0; i < photosNumber; i++) {
-            var randomLikes = Math.ceil(Math.random() * (200 - 15) + 15);
-
-            photoArray[i] = {};
-            photoArray[i].url = 'photos/' + (i + 1) + '.jpg';
-            photoArray[i].likes = randomLikes;
-            photoArray[i].comments = commentsArray.filter(function () {
-                return Math.random() >= 0.5;
-            });
-        }
-
-        return photoArray;
-    }
-
-    function createPhotoElement(photoData) {
-        var photoElement = pictureTemplate.cloneNode(true);
-
-        photoElement.querySelector('img').src = photoData.url;
-        photoElement.querySelector('.picture-comments').textContent = photoData.comments.length;
-        photoElement.querySelector('.picture-likes').textContent = photoData.likes;
-
-        return photoElement;
-    }
+    var picturesContainer = document.querySelector('.pictures');
+    var picturesContainerAll = null;
 
     function addPhotos(containerElement, photoDataArray) {
         var fragment = document.createDocumentFragment();
 
         for (var i = 0; i < photoDataArray.length; i++) {
-            var photoElement = createPhotoElement(photoDataArray[i]);
+            var photoElement = picture.createPhotoElement(photoDataArray[i]);
             fragment.appendChild(photoElement);
         }
 
@@ -53,7 +19,7 @@
     function getUniqueClickListener(photoData) {
         return function (evt) {
             evt.preventDefault();
-            openPicture(photoData);
+            preview.openPicture(photoData);
         }
     }
 
@@ -64,27 +30,14 @@
         }
     }
 
-    function onEscPressUpload(evt) {
-        if (window.isEscape(evt)) {
-            hideUploadOverlay();
-        }
-    }
-
-    function showUploadOverlay(evt) {
-        uploadOverlay.classList.remove('invisible'); //открытие ф. кадрирования
-        document.addEventListener('keydown', onEscPressUpload);
-    }
-
     function init() {
-        var photosObjectData = getPhotosObjects(NUMBER_PHOTOS);
-        hideUploadOverlay();
+        var photosObjectData = data.getData();
         addPhotos(picturesContainer, photosObjectData);
-        openPicture(photosObjectData[0]);
+        preview.openPicture(photosObjectData[0]);
 
         picturesContainerAll = document.querySelectorAll('.picture');
         initEventListenersForPhotos(photosObjectData);
 
-        document.getElementById('upload-file').addEventListener('change', showUploadOverlay);
     }
 
     init();
