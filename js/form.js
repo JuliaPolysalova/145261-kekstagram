73a9585448d202359a59795c1f78b320707f0556
JuliaPolysalova <uliaska2@mail.ru>
2017-04-25 20:28:36 +0300

    pin

--- js/form.js
+++ js/form.js
@@ -8,6 +8,7 @@
     var filterFormResizeInput = filterForm.querySelector('input[type="text"]');
     var filterControls = document.querySelector('.upload-filter-controls');
     var uploadImgForm = document.querySelector('.upload-image');
+    var uploadOverlay = document.querySelector('.upload-overlay');
 
     var uploadForm = document.querySelector('#upload-select-image');
     var uploadFormCancel = document.querySelector('.upload-form-cancel');
@@ -17,38 +18,28 @@
     var MAX_RESIZE = '100%';
     var MIN_RESIZE = '25%';
     var STEP_RESIZE = 25;
+    var ESCAPE_KEY_CODE = 27;
+    var ENTER_KEY_CODE = 13;
 
-    uploadForm.classList.remove('invisible');
+    var currentFilter = null;
+    var currentFilterValue = 1;
 
-    uploadFormCancel.addEventListener('click', function() {
-        hideUploadOverlay();
-    });
+    var filterLevelScale = document.querySelector('.upload-filter-level-line');
+    var filterLevelValue = document.querySelector('.upload-filter-level-val');
+    var filterLevelPin = document.querySelector('.upload-filter-level-pin');
 
-    uploadFormCancel.addEventListener('keydown', function(evt) {
-        if (isEnter(evt)){
-            hideUploadOverlay();
-        };
-    });
+    //var filterCoordinateStart = filterScaleCoordinates.left;
+    //var filterCoordinateEnd = filterScaleCoordinates.right;
+    var filterCoordinateX;
+    var filterScaleCoordinates = filterLevelScale.getBoundingClientRect();
 
-    uploadFormSubmit.addEventListener('keydown', function(evt) {
-        if (isEnter(evt)){
-            hideUploadOverlay();
-        };
-    });
-
-    uploadFormDescription.addEventListener('keydown', function(evt) {
-        if (isEscape(evt)) {
-            evt.stopPropagation();
-        }
-    });
-
-    filterFormPlusBtn.addEventListener('click', function(evt) {
-        setScale(MAX_RESIZE, 1);
-    });
-
-    filterFormMinusBtn.addEventListener('click', function(evt){
-        setScale(MIN_RESIZE, 0);
-    });
+    function clearFilterForm() {
+        uploadImgForm.reset();
+        filterForm.reset();
+        filterFormResizeInput.setAttribute('value', '100%');
+        filterFormPreview.style.transform = 'scale(1)';
+        filterFormPreview.className = 'filter-image-preview';
+    }
 
     function setScale(sizeLimit, sign) {
         var stepSize;
@@ -61,33 +52,157 @@
         }
     }
 
+
+    var onFilterScalePinMousedown = function (evt) {
+        evt.preventDefault();
+        filterCoordinateX = evt.clientX;
+
+        document.addEventListener('mousemove', onFilterScalePinMouseMove);
+        document.addEventListener('mouseup', onFilterScalePinMouseUp);
+    };
+
+    var onFilterScalePinMouseMove = function (evt) {
+        evt.preventDefault();
+        var shiftCoordinateX = filterCoordinateX - evt.clientX;
+        var newValueForElementStyle = filterLevelPin.offsetLeft - shiftCoordinateX + 'px';
+        filterCoordinateX = evt.clientX;
+        if (filterCoordinateX >= filterCoordinateStart && filterCoordinateX <= filterCoordinateEnd) {
+            filterLevelPin.style.left = newValueForElementStyle;
+            filterLevelValue.style.width = newValueForElementStyle;
+            changeFilterLevel(filterCoordinateX);
+        } else if (filterCoordinateX < filterCoordinateStart) {
+            filterCoordinateX = filterCoordinateStart;
+        } else if (filterCoordinateX > filterCoordinateEnd) {
+            filterCoordinateX = filterCoordinateEnd;
+        }
+    };
+
+    var onFilterScalePinMouseUp = function (evt) {
+        evt.preventDefault();
+        document.removeEventListener('mousemove', onFilterScalePinMouseMove);
+        document.removeEventListener('mouseup', onFilterScalePinMouseUp);
+    };
+
+    filterLevelPin.addEventListener('mousedown', onFilterScalePinMousedown);
+
+    function changeFilterLevel(coordinate) {
+        var currentFilter = filterForm.querySelector('input[type=radio]:checked');
+        var filterName = currentFilter.value;
+        var _coordinate = coordinate - filterScaleCoordinates.left;
+        var coeficient = '';
+        var styleFilter = '';
+        var unit = '';
+        switch (filterName) {
+            case 'chrome':
+                coeficient = 1;
+                styleFilter = 'grayscale';
+                break;
+            case 'sepia':
+                coeficient = 1;
+                styleFilter = 'sepia';
+                break;
+            case 'marvin':
+                coeficient = 100;
+                styleFilter = 'invert';
+                unit = '%';
+                break;
+            case 'phobos':
+                coeficient = 3;
+                styleFilter = 'blur';
+                unit = 'px';
+                break;
+            case 'heat':
+                coeficient = 3;
+                styleFilter = 'brightness';
+                break;
+            default:
+                coeficient = '';
+                styleFilter = 'none';
+        }
+        var level = _coordinate / (filterScaleCoordinates.width / coeficient);
+        picturePreview.style.filter = styleFilter + '(' + level + unit + ')';
+    }
+
     function setFilter(evt) {
-        if (evt.target.checked) {
+        if (evt.target.localName === 'label') {
+            evt.target.click();
+        } else if (evt.target.localName === 'input' && evt.target.checked) {
+            if (currentFilter !== evt.target.value) {
+                currentFilterValue = 1;
+                currentFilter = evt.target.value;
+                if(currentFilter == 'none') {
+                    filterControls.classList.add('invisble');
+                } else {
+                    filterControls.classList.remove('invisble');
+                }
+                console.log(currentFilter);
+            }
             filterFormPreview.className = 'filter-image-preview filter-' + evt.target.value;
         }
     }
 
-    filterControls.addEventListener('click', function(evt) {
-        setFilter(evt);
-    });
+    function isEnter(evt) {
+        return evt.keyCode === ENTER_KEY_CODE;
+    }
 
-    filterControls.addEventListener('keydown', function(evt) {
-        if (isEnter(evt)){
-            setFilter();
-            //console.log('hi');
+    function isEscape(evt) {
+        return evt.keyCode === ESCAPE_KEY_CODE;
+    }
+
+    function onEscPressUpload(evt) {
+        if (evt.keyCode === ESCAPE_KEY_CODE) {
+            hideUploadOverlay();
         }
-    });
+    }
 
-    function clearFilterForm() {
-        uploadImgForm.reset();
-        filterForm.reset();
-        filterFormResizeInput.setAttribute('value', '100%');
-        filterFormPreview.style.transform = 'scale(1)';
-        filterFormPreview.className = 'filter-image-preview';
+    function showUploadOverlay(evt) {
+        uploadOverlay.classList.remove('invisible'); //открытие ф. кадрирования
+        document.addEventListener('keydown', onEscPressUpload);
+        filterControls.classList.add('invisble');
     }
 
     function hideUploadOverlay(evt) {
         uploadOverlay.classList.add('invisible');
         clearFilterForm();
     }
+
+    function init () {
+        uploadForm.classList.remove('invisible');
+
+        uploadFormCancel.addEventListener('click', function() {
+            hideUploadOverlay();
+        });
+
+        uploadFormDescription.addEventListener('keydown', function(evt) {
+            if (isEscape(evt)) {
+                evt.stopPropagation();
+            }
+        });
+
+        filterFormPlusBtn.addEventListener('click', function(evt) {
+            setScale(MAX_RESIZE, 1);
+        });
+
+        filterFormMinusBtn.addEventListener('click', function(evt){
+            setScale(MIN_RESIZE, 0);
+        });
+
+        filterControls.addEventListener('click', function(evt) {
+            setFilter(evt);
+        });
+
+        filterControls.addEventListener('keydown', function(evt) {
+            if (isEnter(evt)){
+                //evt.preventDefault();
+                setFilter(evt);
+            }
+        });
+
+        document.getElementById('upload-file').addEventListener('change', showUploadOverlay);
+
+        hideUploadOverlay();
+    }
+
+    init();
+
 })();
