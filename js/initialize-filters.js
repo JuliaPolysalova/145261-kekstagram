50362abf38a53df6ad90b188a7690dacad057566
JuliaPolysalova <uliaska2@mail.ru>
2017-04-28 22:24:39 +0300

    load

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,11 +1,33 @@
 'use strict';
 
-window.initializeFilters = (function (controlsContainer, callback) {
-    controlsContainer.addEventListener('click', function (evt) {
-        if (evt.target.hasAttribute('type')) {
-            var filter = evt.target;
-            var newFilter = 'filter-' + filter.value;
-            callback(newFilter);
+window.initializeFilters = function (controls, callback) {
+    var isEnter = function(evt) {
+        return evt.keyCode === 13;
+    };
+
+    var setFilter = function (evt) {
+        //debugger;
+        var filterFormPreview = document.querySelector('.filter-image-preview');
+        var currentFilter = evt.target;
+
+        if (evt.target.localName === 'label') {
+            evt.target.click();
+        } else if (evt.target.localName === 'input' && evt.target.checked) {
+            if (currentFilter !== evt.target.value){
+                callback(currentFilter);
+            }
+            filterFormPreview.className = 'filter-image-preview filter-' + evt.target.value;
+        }
+    };
+
+    controls.addEventListener('click', function(evt) {
+        setFilter(evt);
+    });
+
+    controls.addEventListener('keydown', function(evt) {
+        if (isEnter(evt)) {
+            setFilter(evt);
         }
     });
-});
+
+};
--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,41 +1,23 @@
 'use strict';
 
-window.initializeScale = (function (scale, plus, minus, maxScale, minScale, scaleStep, cb) {
+window.initializeScale = function (scale, plus, minus, maxScale, minScale, scaleStep, cb) {
     var actualScale = scale;
+
+    minScale = parseInt(minScale, 10);
+    maxScale = parseInt(maxScale, 10);
+    scaleStep = parseInt(scaleStep, 10)
+
     plus.addEventListener('click', function () {
-        if (actualScale !== maxScale) {
+        if (actualScale+scaleStep <= maxScale) {
             actualScale += scaleStep;
         }
         cb (actualScale);
     });
 
     minus.addEventListener('click', function () {
-        if (actualScale !== minScale) {
+        if (actualScale-scaleStep >= minScale) {
             actualScale -= scaleStep;
         }
         cb (actualScale);
-    })
-});
-
-/* var initializeScale = (function (scale, plus, minus, maxScale, minScale, scaleStep, cb) {
- var actualScale = scale;
- plus.addEventListener('click', function () {
- if (actualScale !== maxScale) {
- actualScale += scaleStep;
- }
- cb (actualScale);
- });
-
- minus.addEventListener('click', function () {
- if (actualScale !== minScale) {
- actualScale -= scaleStep;
- }
- cb (actualScale);
- })
- });
-
- initializeScale(100, filterFormPlusBtn, filterFormMinusBtn, MAX_RESIZE, MIN_RESIZE, STEP_RESIZE, function () {
- var sizeValue = parseInt(filterFormResizeInput.value, 10);
- filterFormResizeInput.setAttribute('value', sizeValue + '%');
- filterFormPreview.style.transform = 'scale(' + sizeValue / 100 + ')';
- });*/
+    });
+}
