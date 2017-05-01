52f512a84da3f3c11c44ef57df9a2ccc29c1ac8a
JuliaPolysalova <uliaska2@mail.ru>
2017-05-02 00:24:56 +0300

    8-1

--- js/input.js
+++ js/input.js
@@ -1,28 +1,37 @@
 'use strict';
-
 (function () {
-    var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-
     var fileChooser = document.querySelector('.upload input[type=file]');
     var preview = document.querySelector('.filter-image-preview');
+    var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+    function optimizeSize(picture) {
+        var width = parseInt(picture.width, 10);
+        var height = parseInt(picture.height, 10);
+        if (width > height) {
+            var widthShift = (width - 587) / 2;
+            picture.style.transform = 'translateX(-' + widthShift + 'px)';
+        }
+    }
 
     fileChooser.addEventListener('change', function () {
-        debugger;
         var file = fileChooser.files[0];
+        if (!file) {
+            return;
+        }
         var fileName = file.name.toLowerCase();
-
-        var matches = FILE_TYPES.some(function (it) {
+        var check = FILE_TYPES.some(function (it) {
             return fileName.endsWith(it);
         });
-
-        if (matches) {
+        if (check) {
             var reader = new FileReader();
-
             reader.addEventListener('load', function () {
                 preview.src = reader.result;
+                preview.style.height = '587px';
             });
-
             reader.readAsDataURL(file);
+            reader.addEventListener('loadend', function (evt) {
+                optimizeSize(preview);
+            });
         }
     });
 })();
